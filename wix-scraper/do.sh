07a003b8aec05bc12c1810b90b4bd23cfa95cb36
Axel Karjalainen <axel@axka.fi>
2023-01-19 15:14:54 +0200

    Add a little tutorial to the Wix scraper script.

--- wix-scraper/do.sh
+++ wix-scraper/do.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-readonly instance=${1:?"The instance must be specified."}
+readonly instance=${1:?"The instance must be specified. You can copy the header from your browser's devtools on API requests when visiting huhuu.news."}
 
 curl -H "instance: $instance" 'https://www.huhuu.news/_api/communities-blog-node-api/_api/posts?offset=0&size=500&pinnedFirst=true&excludeContent=false' > posts.json
 curl -H "instance: $instance" 'https://www.huhuu.news/_api/communities-blog-node-api/_api/categories?offset=0&size=500&pinnedFirst=true' > categories.json
@@ -8,4 +8,6 @@ curl -H "instance: $instance" 'https://www.huhuu.news/_api/communities-blog-node
 rm -r output/*
 
 ./draftjs_to_markdown.ts
+
+#cp -r output ../huhuu-astro/src/pages/blog
 
