cf93793a06748379b2c16b7c0ad6d075606d6c89
Axel Karjalainen <axel@axka.fi>
2023-01-29 23:51:19 +0200

    Improve do_nonet_threaded.sh

--- wix-scraper/do_nonet_threaded.sh
+++ wix-scraper/do_nonet_threaded.sh
@@ -3,16 +3,13 @@ set -e
 rm -rf output
 mkdir output
 
-# Currently this is 80 seconds and do_nonet.sh is 20 seconds.
-echo Don\'t use this script > /dev/stderr
-exit 1
-
 if [[ $1 = "--cp-astro" ]]; then
 	shift
 	cp_astro=true
 fi
 
-jq -r '.[].slug' < posts.json | parallel --joblog /dev/stderr ./draftjs_to_markdown.ts --filter-post={} "$@" \> /dev/null
+#jq -r '.[].slug' < posts.json | parallel --joblog /dev/stderr ./draftjs_to_markdown.ts --filter-post={} "$@" \> /dev/null
+jq -r '.[].slug' < posts.json | parallel -j$(($(nproc) * 2)) --bar ./draftjs_to_markdown.ts --filter-post={} "$@" \> /dev/null
 
 if [[ $cp_astro = true ]]; then
 	rm -rf ../huhuu-astro/src/pages/blog
